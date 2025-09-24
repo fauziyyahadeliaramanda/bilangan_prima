ed16273fccf87f72f4a326e28306b0cd3b200ad2
Fauziyyah Adelia Ramanda <framandaadelia@gmail.com>
2025-09-24 11:36:42 +0700

    tambah daftar cek bil prima dan satu angka

--- bin/bilangan_prima.dart
+++ bin/bilangan_prima.dart
@@ -1,42 +1,33 @@
 import 'dart:io';
+import 'dart:math';
+
+bool isPrima(int n) {
+  if (n < 2) return false;
+  for (int i = 2; i <= sqrt(n).toInt(); i++) {
+    if (n % i == 0) return false;
+  }
+  return true;
+}
 
 void main() {
-  print("=== Program Matematika Sederhana ===");
-  print("1. Hitung Faktorial");
-  print("2. Cek Bilangan Prima");
+  print("=== Menu Bilangan Prima ===");
+  print("1. Cek satu angka");
+  print("2. Tampilkan bilangan prima sampai N");
   stdout.write("Pilih menu (1/2): ");
-  int pilih = int.parse(stdin.readLineSync()!);
+  int menu = int.parse(stdin.readLineSync()!);
 
-  if (pilih == 1) {
+  if (menu == 1) {
     stdout.write("Masukkan angka: ");
     int n = int.parse(stdin.readLineSync()!);
-    int faktorial = 1;
-    for (int i = 1; i <= n; i++) {
-      faktorial *= i;
-    }
-    print("Faktorial dari $n adalah $faktorial");
-  } else if (pilih == 2) {
-    stdout.write("Masukkan angka: ");
+    print(isPrima(n) ? "$n adalah bilangan prima" : "$n bukan bilangan prima");
+  } else if (menu == 2) {
+    stdout.write("Masukkan batas angka: ");
     int n = int.parse(stdin.readLineSync()!);
-    bool prima = true;
-
-    if (n < 2) {
-      prima = false;
-    } else {
-      for (int i = 2; i <= n ~/ 2; i++) {
-        if (n % i == 0) {
-          prima = false;
-          break;
-        }
-      }
-    }
-
-    if (prima) {
-      print("$n adalah bilangan prima");
-    } else {
-      print("$n bukan bilangan prima");
+    print("Bilangan prima sampai $n:");
+    for (int i = 2; i <= n; i++) {
+      if (isPrima(i)) print(i);
     }
   } else {
-    print("Menu tidak tersedia.");
+    print("Menu tidak valid!");
   }
 }
